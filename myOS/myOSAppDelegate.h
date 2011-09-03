1fc664fca2eb3dd5eed4048df82192e60f51daef
Pongsakorn Noksapa <manmiho@hotmail.com>
2011-09-03 11:21:13 +0700

    Initial Commit

---
+++ myOS/myOSAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  myOSAppDelegate.h
+//  myOS
+//
+//  Created by Pongsakorn Noksapa on 9/3/11.
+//  Copyright 2011 RSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface myOSAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
